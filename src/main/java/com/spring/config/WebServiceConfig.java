1b500ea16975b7b39e37e9c1b2e0f0c8f56893a6
GDLPratya <pratya.khaday@gmail.com>
2019-06-02 23:09:45 +0700

    edit conflict

--- src/main/java/com/spring/config/WebServiceConfig.java
+++ src/main/java/com/spring/config/WebServiceConfig.java
@@ -29,7 +29,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	    DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
 	    wsdl11Definition.setPortTypeName("CountriesPort");
 	    wsdl11Definition.setLocationUri("/ws");
-	    wsdl11Definition.setTargetNamespace("http://tirmizee.com/ws/countries");
+	    wsdl11Definition.setTargetNamespace("http://tirmizee.com/ws/countries"); 
 	    wsdl11Definition.setSchema(countriesSchema);
 	    return wsdl11Definition;
 	}
