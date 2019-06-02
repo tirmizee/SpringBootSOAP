19c4c32b414912d8066b9e85bbc7294a5a50330d
GDLPratya <pratya.khaday@gmail.com>
2019-06-02 23:08:36 +0700

    init project soap

---
+++ src/main/java/com/spring/endpoint/CountryEndpoint.java
@@ -0,0 +1,28 @@
+package com.spring.endpoint;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.tirmizee.ws.countries.Country;
+import com.tirmizee.ws.countries.GetCountryRequest;
+import com.tirmizee.ws.countries.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+
+	private static final String NAMESPACE_URI = "http://tirmizee.com/ws/countries";
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        Country country = new Country();
+        country.setCapital("11111111");
+        country.setName("3333333333");
+        response.setCountry(country);
+        return response;
+    }
+	
+}
